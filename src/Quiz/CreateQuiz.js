a48676aff5a893af3ca6d21e4be536f59a6bb40a
Vlad Bogus <vbogus@appriss.com>
2023-12-27 16:31:29 +0000

    MOD: Updated position of answers

--- src/Quiz/CreateQuiz.js
+++ src/Quiz/CreateQuiz.js
@@ -5,43 +5,71 @@ import {Button, StyleSheet, Text, View} from 'react-native';
 const CreateQuiz = () => {
   const [questionTextValue, setQuestionTextValue] = useState('');
   const [numberOfAnswers, setNumberOfAnswers] = useState(0);
+  const [answers, setAnswers] = useState([]);
+
   const [questions, setQuestions] = useState([]);
+
   const updateQuestionTextValue = text => {
     setQuestionTextValue(text);
   };
   const updateNumberOfAnswersValue = text => {
-    setNumberOfAnswers(text);
+    const n = parseInt(text, 10);
+    console.log(n);
+    if (!isNaN(n) && n > 0) {
+      setNumberOfAnswers(n);
+      setAnswers(new Array(n).fill({}));
+    } else {
+      setNumberOfAnswers('');
+      setAnswers([]);
+    }
   };
   const showMeTheText = () => {
-    setQuestions([...questions, questionTextValue]);
-    setQuestionTextValue('');
+    if (questionTextValue !== '' && numberOfAnswers > 0) {
+      setQuestions([...questions, questionTextValue]);
+      setQuestionTextValue('');
+      setNumberOfAnswers(0);
+    }
   };
   //   console.log(...questions);
   return (
-    <View>
-      <Text>Let's Create A Quiz!</Text>
-      <View style={styles.inputContainer}>
-        <Text style={styles.inputText}>Enter Queston: </Text>
-        <TextInput
-          value={questionTextValue}
-          style={styles.customTextInput}
-          onChangeText={updateQuestionTextValue}
-        />
-      </View>
-      <View style={styles.inputContainer}>
-        <Text style={styles.inputText}>Enter Number Of Answers: </Text>
-        <TextInput
-          value={numberOfAnswers}
-          style={styles.customTextInput}
-          onChangeText={updateNumberOfAnswersValue}
-        />
-      </View>
-      <Button title="SaveText!" onPress={showMeTheText} />
+    <>
+      <View>
+        <Text>Let's Create A Quiz!</Text>
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputText}>Enter Queston: </Text>
+          <TextInput
+            value={questionTextValue}
+            style={styles.customTextInput}
+            onChangeText={updateQuestionTextValue}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputText}>Enter Number Of Answers: </Text>
+          <TextInput
+            value={numberOfAnswers}
+            style={styles.customTextInput}
+            onChangeText={updateNumberOfAnswersValue}
+          />
+        </View>
 
-      {questions.map(a => {
-        return <Text>{JSON.stringify(a)}</Text>;
-      })}
-    </View>
+        <Button title="SaveText!" onPress={showMeTheText} />
+
+        {questions.map(a => {
+          return <Text>{JSON.stringify(a)}</Text>;
+        })}
+      </View>
+
+      <View style={styles.answersContainer}>
+        {answers.map((a, i) => {
+          return (
+            <View style={styles.answerRow}>
+              <Text style={styles.answersInputText}>Answer {i}:</Text>
+              <TextInput style={styles.customTextInput} value="" />
+            </View>
+          );
+        })}
+      </View>
+    </>
   );
 };
 
@@ -60,6 +88,17 @@ const styles = StyleSheet.create({
     textAlignVertical: 'center',
   },
   inputText: {
+    flex: 1,
+  },
+  answersContainer: {
+    flex: 1,
+    width: 600,
+  },
+  answerRow: {
+    flexDirection: 'row',
+    textAlignVertical: 'center',
+  },
+  answersInputText: {
     flex: 1,
   },
 });
